c6b4ee9dcedb50e587755a52c6d43eb256bb8508
Marcus Sera <m.yoshiosera@gmail.com>
2020-10-03 02:38:49 -0700

    styling syntax

--- client/src/components/ActivityDescriptionTable.vue
+++ client/src/components/ActivityDescriptionTable.vue
@@ -25,8 +25,8 @@ export default {
       titleArray: [
         'Name',
         'Date / Time',
-        'Distance',
-        'Moving Time',
+        'Distance (Miles)',
+        'Moving Time (hr:min)',
         'Elapsed Time',
         'Average Speed',
       ],
@@ -40,27 +40,62 @@ export default {
         if (this.propsActivityName === i['type']) {
           newArr.push(i['name']);
           let date = i['start_date_local'].split('T')[0];
-          let formattedDate = date
-            .split('-')
-            .slice(1)
-            .concat(date.split('-').slice(0, 1))
-            .join('/');
+          let formattedDate = this.formatDate(date);
           let time = i['start_date_local'].split('T')[1].split('Z')[0];
-          let formattedTime = time.split(':');
-          formattedTime.pop();
-          if (parseInt(formattedTime[0]) > 12) {
-            formattedTime[0] = (parseInt(formattedTime[0]) - 12).toString();
-            newArr.push(`${formattedDate} ${formattedTime.join(':') + ' PM'}`);
-          } else {
-            newArr.push(`${formattedDate} ${formattedTime.join(':') + ' AM'}`);
-          }
-          newArr.push(i['distance']);
-          newArr.push(i['moving_time']);
+          let formattedTime = this.formatDateTime(time);
+          newArr.push(`${formattedDate} ${formattedTime}`);
+          let distance = this.formatDistance(i['distance']);
+          newArr.push(distance);
+          let movingTime = this.formatTime(i['moving_time']);
+          newArr.push(movingTime);
           newArr.push(i['elapsed_time']);
           newArr.push(i['average_speed']);
         }
       }
       return newArr;
+    },
+  },
+  methods: {
+    formatDate(date) {
+      let newDate = date.split('-');
+      let day = newDate.slice(2)[0];
+      let month = newDate.slice(1, 2)[0];
+      if (parseInt(month) < 10) {
+        month = month.split('0')[1];
+      }
+      let year = newDate.slice(0, 1)[0];
+      let formattedDate = `${month}/${day}/${year}`;
+      return formattedDate;
+    },
+    formatDateTime(time) {
+      let formattedTime = time.split(':');
+      formattedTime.pop();
+      if (parseInt(formattedTime[0]) > 12) {
+        formattedTime[0] = (parseInt(formattedTime[0]) - 12).toString();
+        return formattedTime.join(':') + ' PM';
+      } else {
+        return formattedTime.join(':') + ' AM';
+      }
+    },
+    formatDistance(distance) {
+      let formattedDistance = (
+        Math.round((parseInt(distance) / 1609.34) * 100) / 100
+      ).toString();
+      return formattedDistance;
+    },
+    formatTime(time) {
+      let formattedTime = Math.round((parseInt(time) / 60 / 60) * 100) / 100;
+      // not accounting for seconds! When I have time, come back to solve
+      if (formattedTime < 1) {
+        formattedTime = (60 * formattedTime).toString() + ' min';
+      } else if (formattedTime > 1 && formattedTime % 1 != 0) {
+        let minutes =
+          (parseInt(formattedTime.toString().split('.')[1]) / 100) * 60;
+        formattedTime = `${formattedTime.toString().split('.')[0]}:${minutes}`;
+      } else {
+        formattedTime = formattedTime.toString() + ' hr';
+      }
+      return formattedTime;
     },
   },
 };
