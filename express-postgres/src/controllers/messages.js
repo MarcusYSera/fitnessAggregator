feaf5a8526af4aba532005ce2dffc0652d040d02
Marcus Sera <m.yoshiosera@gmail.com>
2020-10-10 16:18:17 -0700

    updated syntax

--- express-postgres/src/controllers/messages.js
+++ express-postgres/src/controllers/messages.js
@@ -4,7 +4,7 @@ const messagesModel = new Model('messages');
 
 export const messagesPage = async (req, res) => {
   try {
-    const data = await messagesModel.select('name, messages');
+    const data = await messagesModel.select('name, message');
     res.status(200).json({ messages: data.rows });
   } catch (err) {
     res.status(200).json({ messages: err.stack });
