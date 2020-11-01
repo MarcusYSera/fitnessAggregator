619bf2df4b9ed7aa48f9606da779f216680112a6
Marcus Sera <m.yoshiosera@gmail.com>
2020-10-31 19:25:10 -0700

    save prior to gauth continuation

--- client/src/api/googleAPI.js
+++ client/src/api/googleAPI.js
@@ -8,6 +8,19 @@ export default {
         return 'failed auth';
       }
       if (!gapi.auth) {
+        gapi.client
+          .init({
+            apiKey: 'AIzaSyArIlQZWnUYfbZ-MDyKYi88wXobgcMawu8',
+            clientId:
+              '701476865706-ebqg4cvt0d09pb6egr4b56054s1i2kbk.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/fitness.activity.read',
+          })
+          .then(function() {
+            let GoogleAuth = gapi.auth2.getAuthInstance();
+            console.log(GoogleAuth);
+            // Listen for sign-in state changes.
+            // GoogleAuth.isSignedIn.listen(updateSigninStatus);
+          });
         console.log('gapi loaded, but not authorized yet, set up auth process');
         return 'success';
       }
