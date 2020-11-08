154a1466e2f626607b30220598038a26b5eb88f8
Marcus Sera <m.yoshiosera@gmail.com>
2020-11-07 23:26:38 -0800

    waiting on google to update my redirect uri

---
+++ client/src/components/GoogleCodeHandler.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Google URI Redirect for sign in</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoogleCodeHandler',
+};
+</script>
+
+<style scoped></style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import Home from '../components/Home';
 import Strava from '../components/Strava';
 import StravaCodeHandler from '../components/StravaCodeHandler';
+import GoogleCodeHandler from '../components/GoogleCodeHandler';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/strava', component: Strava },
   { path: '/oauth/callback', component: StravaCodeHandler },
+  { path: '/oauth/googleCallback', component: GoogleCodeHandler },
 ];
 
 const router = createRouter({
