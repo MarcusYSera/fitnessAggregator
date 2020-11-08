eaab30694a66cb45a0dde28bfa320a53f18ed1bb
Marcus Sera <m.yoshiosera@gmail.com>
2020-11-07 18:20:13 -0800

    finially got async await working with gapi

--- client/src/api/googleAPI.js
+++ client/src/api/googleAPI.js
@@ -6,32 +6,15 @@ export default {
   async loadGapi() {
     await gapiPromise;
     const gapi = window.gapi;
-    gapi.load('client:auth2').then(() => {
-      gapi.client.init({
-        clientId: `${process.env.VUE_APP_GOOGLE_CLIENT_ID}`,
-        scope: 'https://www.googleapis.com/auth/fitness.activity.read',
-      });
+    await new Promise((res, rej) => {
+      gapi.load('client:auth2', { callback: res, onerror: rej });
     });
-    // await gapi.load('client:auth2', async () => {
-    //   await gapi.client.init({
-    //     // apiKey: `${process.env.VUE_APP_GOOGLE_API_KEY}`,
-    //     clientId: `${process.env.VUE_APP_GOOGLE_CLIENT_ID}`,
-    //     scope: 'https://www.googleapis.com/auth/fitness.activity.read',
-    //   });
-    //   // console.log('gapi initialized');
-    //   console.log(gapi.auth2.getAuthInstance().isSignedIn.get());
-    //   //   return;
-    // });
-
-    // await gapi.load('client:auth2');
-    // console.log('load finished');
-    // await gapi.client.init({
-    //   // apiKey: `${process.env.VUE_APP_GOOGLE_API_KEY}`,
-    //   clientId: `${process.env.VUE_APP_GOOGLE_CLIENT_ID}`,
-    //   scope: 'https://www.googleapis.com/auth/fitness.activity.read',
-    // });
-    console.log('success');
-    // console.log(gapi.auth2.getAuthInstance().isSignedIn.get());
+    await gapi.client.init({
+      apiKey: `${process.env.VUE_APP_GOOGLE_API_KEY}`,
+      clientId: `${process.env.VUE_APP_GOOGLE_CLIENT_ID}`,
+      scope: 'https://www.googleapis.com/auth/fitness.activity.read',
+    });
+    console.log(gapi.auth2.getAuthInstance().isSignedIn.get());
   },
   checkStatus() {
     const gapi = window.gapi;
